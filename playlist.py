2abd9d0cd53259b186947efc96c722ee1355a420
Meysam Birgan <saturninsky@gmail.com>
2023-08-08 22:42:04 +0000

    improved for loop

--- playlist.py
+++ playlist.py
@@ -2,25 +2,26 @@ import json
 #import sys
 import requests
 
-with open('telegram-music-group-history.json', 'r', encoding="utf8") as json_file:
+with open('telegram-sample.json', 'r', encoding="utf8") as json_file:
     msg_list_json = json.load(json_file)
 
 search_pattern = ''
 
-for i in range(1003,1007):
-    if 'title' in msg_list_json['messages'][i]:
-        search_pattern = search_pattern + msg_list_json['messages'][i]['title']   
-    if 'performer' in msg_list_json['messages'][i]:
-        search_pattern = search_pattern + ' ' + msg_list_json['messages'][i]['performer']
-    if search_pattern:
-        edited_pattern=search_pattern.replace(" ","+")
-        url = f"https://api.spotify.com/v1/search?q={edited_pattern}&type=track"
-        print(url)
-        header = {'Authorization': 'Bearer BQAkBmGui_bxAP5CGdxPUCyIgjTt7VAifc2OAM8Sr4B_HOeZeAO_J1pJbKUASRoiSdPUnVz4qxyEzNE8IYySlj7mnxLbRHu8rXzgOHRyjr7QC7d1fpI'}
-        res = requests.get(url, headers=header)
-        a=res.json()
-        print(a)
-        search_pattern = ''
+for i in range(0,len(msg_list_json['messages'])):
+    if msg_list_json['messages'][i].media_type:
+        if 'title' in msg_list_json['messages'][i]:
+            search_pattern = search_pattern + msg_list_json['messages'][i]['title']   
+        if 'performer' in msg_list_json['messages'][i]:
+            search_pattern = search_pattern + ' ' + msg_list_json['messages'][i]['performer']
+        if search_pattern:
+            edited_pattern=search_pattern.replace(" ","+")
+            url = f"https://api.spotify.com/v1/search?q={edited_pattern}&type=track"
+            print(url)
+            header = {'Authorization': 'Bearer BQAuskvuytPwQUKy5VTs6w3690Nl7R4-nbmIXBiOdqR9ozybdm_g-MSh03ff37unWOvzMkDKouacnWhDIVSuFabhI1J18r8M6dTUq4sYMeB7tfLX_QE'}
+            res = requests.get(url, headers=header)
+            a=res.json()
+            print(a)
+    search_pattern = ''
 
 
 ############## piece of codes that may be useful
