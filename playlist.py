d458e5d015d3b9d36f9428f6fb85817689189719
meysam.birgan <meysam.birgan@ballysinteractive.com>
2023-08-18 19:14:55 -0400

    change authorization to a function

--- playlist.py
+++ playlist.py
@@ -6,11 +6,51 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 global CLIENT_ID
 global CLIENT_SECRET
+global redirect_uri
 
 CLIENT_ID = "c7534310526246c3966dbc89a94ca3e5"
 CLIENT_SECRET = "6b19f6272cbc4e39b8e4944afe6975e7"
 playlist_id = "2QWKUjBiNjnki6OJxyeYi1"
-redirect_uri = "https://open.spotify.com"
+redirect_uri = "http://localhost:8888/callback"
+
+
+def authorize_account():
+
+    # # Set your Spotify API credentials
+    # SPOTIPY_CLIENT_ID = 'your_client_id'
+    # SPOTIPY_CLIENT_SECRET = 'your_client_secret'
+    # SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'  # This should match your app's redirect URI
+
+    # Initialize the SpotifyOAuth object
+    sp_oauth = SpotifyOAuth(
+        CLIENT_ID, 
+        CLIENT_SECRET, 
+        redirect_uri, 
+        scope='playlist-modify-public'  # Add any required scopes here
+    )
+
+    # Get the authorization URL
+    auth_url = sp_oauth.get_authorize_url()
+
+    print("Please navigate to this URL and authorize the app:")
+    print(auth_url)
+
+    # After authorization, the user will be redirected to the redirect URI with a code
+    code = input("Enter the code from the redirect URI: ")
+
+    # Exchange the code for an access token
+    token_info = sp_oauth.get_access_token(code)
+    access_token = token_info['access_token']
+
+    # Initialize the Spotipy object with the access token
+    sp = spotipy.Spotify(auth=access_token)
+
+    # Now you can use the Spotipy object to make authorized API requests
+    user_info = sp.me()
+    print("Logged in as:", user_info['display_name'])
+
+    return sp
+
 
 def renew_bearer_token():
 
@@ -41,15 +81,17 @@ headers1 = {
         'Authorization': 'Bearer {token}'.format(token=access_token)
     }
 
-headers2 = {
-        'Authorization': 'Bearer {token}'.format(token=access_token),
-        'Content-Type': 'application/json'
-    }
+# headers2 = {
+#         'Authorization': 'Bearer {token}'.format(token=access_token),
+#         'Content-Type': 'application/json'
+#     }
 
 with open('telegram-sample2.json', 'r', encoding="utf8") as json_file:
     msg_list_json = json.load(json_file)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri, scope="playlist-modify-public"))
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri, scope="playlist-modify-public"))
+
+sp = authorize_account()
 
 for i in range(0,len(msg_list_json["messages"])):
     if "media_type" in msg_list_json["messages"][i]:
