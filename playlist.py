3f2ca8a9e751e3320d6bed97e0aa6d1c3e8e8180
meysam.birgan <meysam.birgan@ballysinteractive.com>
2023-08-23 16:25:11 -0400

    fix scope issue

--- playlist.py
+++ playlist.py
@@ -26,11 +26,12 @@ def authorize_account():
         CLIENT_ID, 
         CLIENT_SECRET, 
         redirect_uri, 
-        scope='playlist-modify-public'  # Add any required scopes here
+        scope='playlist-modify-private'  # Add any required scopes here
     )
 
     # Get the authorization URL
     auth_url = sp_oauth.get_authorize_url()
+    print(sp_oauth.parse_auth_response_url(auth_url))
 
     print("Please navigate to this URL and authorize the app:")
     print(auth_url)
@@ -41,57 +42,29 @@ def authorize_account():
     # Exchange the code for an access token
     token_info = sp_oauth.get_access_token(code)
     access_token = token_info['access_token']
-
-    # Initialize the Spotipy object with the access token
-    sp = spotipy.Spotify(auth=access_token)
-
-    # Now you can use the Spotipy object to make authorized API requests
-    user_info = sp.me()
-    print("Logged in as:", user_info['display_name'])
-
-    return sp
-
-
-def renew_bearer_token():
-
-    AUTH_URL = "https://accounts.spotify.com/api/token"
-    auth_response = requests.post(AUTH_URL, {
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-    })
-
-    #Convert response to JSON
-    auth_response_data = auth_response.json()
-
-    #Save the access token
-    access_token = auth_response_data['access_token']
-    # print(access_token) - each a new access token would be generated
-
-    #Need to pass access token into header to send properly formed GET request to API server
-    # headers = {
-    #     'Authorization': 'Bearer {token}'.format(token=access_token)
-    # }
+    # print(access_token)
 
     return access_token
 
-access_token = renew_bearer_token()
+access_token = authorize_account()
 
-headers1 = {
-        'Authorization': 'Bearer {token}'.format(token=access_token)
+# Initialize the Spotipy object with the access token
+sp = spotipy.Spotify(auth=access_token)
+
+    # Now you can use the Spotipy object to make authorized API requests
+user_info = sp.me()
+print("Logged in as:", user_info['display_name'])
+
+headers = {
+        'Authorization': 'Bearer {token}'.format(token=access_token),
+        'Content-Type': 'application/json'
     }
 
-# headers2 = {
-#         'Authorization': 'Bearer {token}'.format(token=access_token),
-#         'Content-Type': 'application/json'
-#     }
-
-with open('telegram-sample2.json', 'r', encoding="utf8") as json_file:
+with open('telegram-sample.json', 'r', encoding="utf8") as json_file:
     msg_list_json = json.load(json_file)
 
 # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri, scope="playlist-modify-public"))
 
-sp = authorize_account()
 
 for i in range(0,len(msg_list_json["messages"])):
     if "media_type" in msg_list_json["messages"][i]:
@@ -100,24 +73,32 @@ for i in range(0,len(msg_list_json["messages"])):
         if "performer" in msg_list_json["messages"][i]:
             search_pattern = search_pattern + ' ' + msg_list_json["messages"][i]["performer"]
         if search_pattern:
+            print(f"Searching for {search_pattern}")
             edited_pattern=search_pattern.replace(" ","+")
             getUrl = f"https://api.spotify.com/v1/search?q={edited_pattern}&type=track"
-            print(getUrl)
-            res = requests.get(getUrl, headers=headers1)
+            res = requests.get(getUrl, headers=headers)
 
             song_id = (res.json()["tracks"]["items"][0]["id"])
-            song_uri = [f"spotify:track:{song_id}"]
-            print(song_uri)
+            song_uri = f"spotify:track:{song_id}"
+    
+            # sp.playlist_add_items(playlist_id, [song_uri], position=0)
+            # sp.playlist_items(playlist_id, fields=None, limit=100, offset=0, market=None, additional_types=('track'))
 
-            sp.playlist_add_items(playlist_id, song_uri)
+            postUrl = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+            print(postUrl)
+            trackData = {
+                            "uris": [
+                                song_uri
+                            ],
+                            "position": 0
+                        }
+            addRes = requests.post(postUrl, headers=headers, data=json.dumps(trackData))
+            print(addRes.json()+"\n\n")
 
-            # postUrl = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?uris=spotify%3Atrack%3A{song_uri}"
-            # addRes = requests.post(postUrl, headers=headers2, data=trackData)
-            # print(addRes.json())
             # with open('output.json', 'w') as out_file:
             #     json.dump(res.json(), out_file, sort_keys = True, indent = 4, ensure_ascii = False)
     else:
-        print(f"message number {i} does not contain any media")
-        search_pattern = ''
+        print(f"message number {i} does not contain any media\n\n")
+        # search_pattern = ''
 
-    print("\n\n\n\nAll songs are added")
+print("\n\n\n\nAll songs are added")
